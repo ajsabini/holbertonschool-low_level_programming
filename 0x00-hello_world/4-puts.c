2de5ae01b4f6dec7d5738b2ee9e076f3ad3ed7fb
Alvaro <ajsabini@gmail.com>
2022-02-03 09:01:20 -0800

    aa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main () {
-
+int main (void) {// this specifies there are no arguments taken by main
+	/* declaro la variable chr, y le asigno el string a imprimir */
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	     
-	 puts(Mystr);|
-
-	 return(0);
+	 /*llamo la funcion puts y le paso el parametro*/
+	 puts(Mystr);
+	 return (0);
 }
