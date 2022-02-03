2414b2445687d00e8821b3a2b44ec61eefb3f49e
Alvaro <ajsabini@gmail.com>
2022-02-03 09:37:26 -0800

    fbdsh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include <nstdio.h>
 
 /**
  * main - algo
  * Return: siempre 0
  */
 
-int main (void)
+int main(void)
 {
 	/* declaro la variable chr, y le asigno el string a imprimir */
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 	 /*llamo la funcion puts y le paso el parametro*/
 	 puts(Mystr);
-	 return (0);
+return (0);
 }
