2ce72dada5285ebeb68a4f2c8b8848c9eef02b93
Alvaro <ajsabini@gmail.com>
2022-02-04 05:45:14 -0800

    if + else + else if

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+* main - funcion para saber si es positivo/negativo con if
+* Return: 0, hacemos un printf para ver que devuelve la condicion
+*/
+int main(void)
+{
+		int n;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		/* your code goes there */
+
+		if (n > 0)
+		{
+			return ("is positive");
+		}
+		else if (n < 0)
+		{
+			return ("is negative");
+		}
+		else
+		{
+			return ("is zero");
+		}
+
+		return (0);
+}
