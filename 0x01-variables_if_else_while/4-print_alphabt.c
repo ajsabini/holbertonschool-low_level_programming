48023f4769bfa4a9ba6f57aded52fff96304ac99
Alvaro <ajsabini@gmail.com>
2022-02-04 16:56:29 -0800

    print a to z -e and -q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - funcion para imprimir de la a la z en minusula
+ * Return: 0, hacemos un printf para imprimir letra a letra
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+		}
+		else
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
