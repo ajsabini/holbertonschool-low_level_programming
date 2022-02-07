0698f9a698fcf05045f7c42e24b6e834fa9388ea
Alvaro <ajsabini@gmail.com>
2022-02-07 10:32:06 -0800

    obtener ultimo digito

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_last_digit - pasar a valor absoluto
+ * @n: el numero a chequear
+ * Return: 0 siempre
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	return (n);
+
+}
