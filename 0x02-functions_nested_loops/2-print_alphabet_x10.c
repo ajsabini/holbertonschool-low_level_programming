947f53bb1e6ce4dbcf913872157fb11021936e7c
Alvaro <ajsabini@gmail.com>
2022-02-07 08:42:37 -0800

    print a-z*10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "_putchar.c"
+#include "main.h"
 
 /**
  * print_alphabet_x10 - imprime de la a la z 10 veces
@@ -20,15 +20,4 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 
 	}
-}
-
-/**
-* main - algo
-* Return: siempre 0
-*/
-
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
 }
