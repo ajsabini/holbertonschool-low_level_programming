fda0cec211c549aedf3b0f80bd9e077c260d2f72
Alvaro <ajsabini@gmail.com>
2022-02-07 10:14:08 -0800

    valor absoluto

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - pasar a valor absoluto
+ * @n: el numero a chequear
+ * Return: 0 siempre
+ */
+
+int _abs(int n)
+{
+	n = abs(n);
+
+	return (n);
+
+}
