289f7b0a409af11789dc3f2269ac36785d2c6563
Alvaro <ajsabini@gmail.com>
2022-02-09 09:14:38 -0800

    ej 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - imprime la diagonal
+ */
+
+void print_diagonal(int n)
+{
+	int esp;
+	int bsl;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (bsl = 1; bsl <= n; bsl++)
+		{
+			_putchar('\\');
+			_putchar('\n');
+
+			if(bsl < n)
+			{
+
+				for (esp = 1; esp <= bsl; esp++)
+					_putchar(' ');
+		
+			}
+		}
+	}
+
+}
