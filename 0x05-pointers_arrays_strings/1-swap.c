f53a79df707d31a9982af9c77332b53ea6c55ec7
Alvaro <ajsabini@gmail.com>
2022-02-11 08:27:40 -0800

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - imprime la diagonal
+ * @a: valor
+ * @b: segundo valor
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;	
+	*a = *b;
+	*b = c;
+	
+}
