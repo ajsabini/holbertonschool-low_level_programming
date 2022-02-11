598f4f6cdef5b01b55ee5eb79068a4c67d0bac36
Alvaro <ajsabini@gmail.com>
2022-02-11 08:44:20 -0800

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - imprime la diagonal
+ * @s: valor
+ * Return: devuelve el largo
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
