9644d283b21ad01049cc4cac7fe41d7c25dd1cce
Alvaro <ajsabini@gmail.com>
2022-02-14 06:42:56 -0800

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,25 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * puts_half - imprim la segunda mitad de la cadena
+ * @str: lo que nos pasan como parametro
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i == n)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+}
