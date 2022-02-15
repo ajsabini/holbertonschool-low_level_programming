7bc161683bc425dce7f89ccb616821a33d4b680b
Alvaro <ajsabini@gmail.com>
2022-02-15 04:23:22 -0800

    ej 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - imprime la diagonal
+ * @dest: valor
+ * @src: segundo valor
+ * Return: la copia
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
