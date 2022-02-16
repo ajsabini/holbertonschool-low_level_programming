cd38ade64e1b0f2ab35d4a7ba4dfb50ff7f065d1
Alvaro <ajsabini@gmail.com>
2022-02-16 05:12:51 -0800

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,15 +13,11 @@ void reverse_array(int *a, int n)
 {
 	int x;
 	int temporal;
-	for (x = 0; x < n / 2; x++) {
-	  
-	temporal = a[x];
-
-
-
-	a[x] = a[n - x - 1];
-
-	a[n - x - 1] = temporal;
-	                                            }
-
+	
+	for (x = 0; x < n / 2; x++)
+	{
+		temporal = a[x];
+		a[x] = a[n - x - 1];
+		a[n - x - 1] = temporal;
+	}
 }
