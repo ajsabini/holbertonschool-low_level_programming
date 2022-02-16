7b2a0479240eac6580ab563b63c6d8a0b7987460
Alvaro <ajsabini@gmail.com>
2022-02-16 06:56:05 -0800

    a

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * cap_string - da vuelta un string
+ * @c: puntero que trae el array de enteros
+ * Return: la copia
+ */
+
+char *cap_string(char *c)
+{
+	size_t i;
+	int n = 0;
+
+	for (i = 0; i < strlen(c); i++)
+	{
+		if (n == 0 && (c[i] > 96 && c[i] < 123))
+		{
+			c[i] = c[i] - 32;
+			n++;
+		}
+
+		if (c[i] == ',' || c[i] == ';' || c[i] == '.' || c[i] == '!' || c[i] == '?' || c[i] == '"' || c[i] == '(' || c[i] == ')' || c[i] == '{' || c[i] == '}' || c[i] == ' ' || c[i] == 9 || c[i] == 10)
+		{
+			n = 0;
+		}
+	}
+	return (c);
+}
