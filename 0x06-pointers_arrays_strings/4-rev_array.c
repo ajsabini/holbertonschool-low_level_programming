ec7df37b36a53eb1e54a5dc26b15d66829847c39
Alvaro <ajsabini@gmail.com>
2022-02-16 05:09:53 -0800

    aa

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 
 /**
  * reverse_array - da vuelta un string
@@ -10,12 +11,17 @@
 
 void reverse_array(int *a, int n)
 {
-	int i;
-	int aux[1024];
+	int x;
+	int temporal;
+	for (x = 0; x < n / 2; x++) {
+	  
+	temporal = a[x];
 
-	for (i = 0; i < strlen(*a); i++)
-	{
-		aux[i] = a[n];
-		n--;
-	}
+
+
+	a[x] = a[n - x - 1];
+
+	a[n - x - 1] = temporal;
+	                                            }
+
 }
