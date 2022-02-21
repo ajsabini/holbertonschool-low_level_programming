608233177f2fe1dc3e3aca5e589c773b2d4dfc4e
Alvaro <ajsabini@gmail.com>
2022-02-21 09:52:47 -0800

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - encontrar caracter
+ * @s: puntero con string
+ * @accept: caracter a encontrar
+ * Return: total
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int k;
+	int n = 0;
+
+	for (i = 0; accept[i]; i++)
+	{
+		for (k = 0; s[k]; k++)
+		{
+			if (s[k] == 44)
+				break;
+			
+			if (accept[i] == s[k])
+			{
+				n++;
+			}
+		}
+	}
+
+	return (n);
+}
