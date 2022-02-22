6f7251974cff0b9df7b061113d410b7cabf05bc5
Alvaro <ajsabini@gmail.com>
2022-02-21 17:05:44 -0800

    ej 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,32 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _strstr - encontrar un string en una cadena
  * @haystack: la cadena
  * @needle: el string
  * Return: NULL si no encuentra o lo que coincide
  */
+
 char *_strstr(char *haystack, char *needle)
 {
-        int i;
-        int k = 0;
+	int i;
+	int k = 0;
 
-        for (; needle[k] ;)
-                k++;
+	for (; needle[k] ;)
+		k++;
 
-        for (; haystack != NULL;)
-        {
-                for (i = 0; needle[i]; i++)
-                {
-                        if (haystack[i] != needle[i])
-                                break;
-                }
-                if (i != k)
-                        haystack++;
-                else
-                        return (haystack);
-        }
-        return (NULL);
+	for (; haystack != NULL;)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+			if (haystack[i] != needle[i])
+				break;
+		}
+		if (i != k)
+			haystack++;
+		else
+			return (haystack);
+	}
+	return (NULL);
 }
