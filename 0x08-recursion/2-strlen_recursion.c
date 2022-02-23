49d681a4c3cd01989557c984cf98350cea71105d
Alvaro <ajsabini@gmail.com>
2022-02-23 06:15:59 -0800

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,19 +3,19 @@
 #include <string.h>
 
 /**
- * _puts_recursion - imprimir un string seguido de una linea
+ * _strlen_recursion - imprimir un string seguido de una linea
  * @s: el string
  * Return - void
  */
 
 int _strlen_recursion(char *s)
 {
-        if (*s == 0)
-        {
-		return (0);
-        }
-        else
+	if (*s == 0)
 	{
-                return (1 + _strlen_recursion(s + 1));
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
