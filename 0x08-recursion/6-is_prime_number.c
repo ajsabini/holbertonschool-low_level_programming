69558b7ae573baa1385ff1ec68ef846df370ad10
Alvaro <ajsabini@gmail.com>
2022-02-23 17:49:55 -0800

    ej 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,34 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 #include <string.h>
 
 /**
- * is_prime_number - chequear si es un numero primo
- * @n: el numero a evaluar
- * Return: 1 si es primo, 0 si no
+ * __prime - enontrar numero primo
+ * @n: entero
+ * @k: entero
+ * Return: el primo
  */
 
+int __prime(int n, int k)
+{
+	if (k == 1)
+		return (1);
+	else if ((n % k) == 0)
+		return (0);
+	else
+		return (__prime(n, k - 1));
+}
+
+
+/**
+ * is_prime_number - saber si es numero primo
+ * @n: el numero a evaluar
+ * Return: 1 si es primo, 0 si no es
+ */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else if (n % 2 == 0)
-	{
-		if (n == 2)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-	}
+	if (n > 1)
+		return (__prime(n, n - 1));
 	else
-	{
-		
-	}
+		return (0);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,3 +7,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _putchar(char);
 int __sqrt(int n, int k);
+int __prime(int n, int k);
