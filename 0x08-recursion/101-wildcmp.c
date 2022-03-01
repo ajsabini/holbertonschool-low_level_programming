ae464a16692e722a38049e6860967730b387a8ec
Alvaro <ajsabini@gmail.com>
2022-03-01 15:52:56 -0800

    wild

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,40 +1,35 @@
 #include <stdio.h>
-#include "main.h"
 #include <string.h>
 
 /**
- * wildcmp - saber si es numero primo
- * @s1: 
- * @s2:
- * Return: 1 si es primo, 0 si no es
+ * wildcmp - comparar dos strings
+ * @s1: str 1
+ * @s2: str 2
+ * Return: 1 o 0
  */
-int wildcmp(char *s1, char *s2)
+
+int wildcmp (char *s1, char *s2)
 {
-if (s1)
-{
-	if (s1 == s2)
+
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	if (*s1 == '\0' && *s2 != '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	else
+	if (*s2 == '*')
 	{
-		if (*s2 != '*')
-		{
-			return (0);
-		}
-		else
-		{
-			if (s2)
-			{
-				return (wildcmp(s1, s2 + 1));
-			}
-			else
-			{
-				return 0;
-			}
-		}	
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1, s2 + 1) != '\0')
+			return (1);
+		return (wildcmp(s1 + 1, s2));
 	}
-	return (1);
-}
-return (0);
+	return (0);
 }
