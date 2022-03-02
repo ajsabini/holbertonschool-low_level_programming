e2fae42ab0789b78a126c13bea7d028da8421dd2
Alvaro <ajsabini@gmail.com>
2022-03-02 09:51:54 -0800

    aaaa

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * int **alloc_grid - imprimir un string seguido de una linea
+ * @width: el string
+ * @height: dafs
+ * Return: void
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **doble;
+	int i;
+	
+	doble = (int **)malloc(width*sizeof(int*));/* this will store base order of all the row in p */
+	for(i = 0; i < height; i++)
+		doble[i] = (int *)malloc(height*sizeof(int));/* this will create m row of n elements */
+
+	return (doble);
+}
