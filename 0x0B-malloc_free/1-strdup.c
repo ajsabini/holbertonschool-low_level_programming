c6aa26a7783eae54cb8262fec7e57d881b31a951
Alvaro <ajsabini@gmail.com>
2022-03-02 04:59:08 -0800

    ej 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * char *_strdup - imprimir un string seguido de una linea
+ * @str: el string
+ * Return: void
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicado;
+	int largo = 0;
+	int i;
+
+	while (str[largo] != '\0')
+		largo++;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (1)
+		{
+			duplicado = malloc(sizeof(char) * largo);
+
+			for (i = 0; i < largo && str[i] != '\0'; i++)
+				duplicado[i] = str[i];
+			for ( ; i < largo; i++)
+				duplicado[i] = '\0';
+			return (duplicado);
+		}
+	}
+	return (NULL);
+}
