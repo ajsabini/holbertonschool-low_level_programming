4ce0f3deb13d505e90b15fccc53beae97e0027bb
Alvaro <ajsabini@gmail.com>
2022-03-02 07:38:05 -0800

    aaa

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,39 +17,30 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	int k;
 
-		if (s1 == NULL)
-			s1 = "";
-		if (s2 == NULL)
-			s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-		while (s1[largo1] != '\0')
-	        {
-			largo1++;
-		}
+	while (s1[largo1] != '\0')
+		largo1++;
 
-		while (s2[largo2] != '\0')
-		{
-			largo2++;
-		}
+	while (s2[largo2] != '\0')
+		largo2++;
 
-		while (1)
-		{
-			concatenados = malloc(sizeof(char) * (largo1 + largo2 + 1));
-			if (concatenados == NULL)
-			{
-				return (NULL);
-			}
+	concatenados = malloc(sizeof(char) * (largo1 + largo2 + 1));
+	if (concatenados == NULL)
+	{
+		return (NULL);
+	}
 
-			for (i = 0; i < largo1 && s1[i] != '\0'; i++)
-				concatenados[i] = s1[i];
+	for (i = 0; i < largo1 && s1[i] != '\0'; i++)
+		concatenados[i] = s1[i];
 
-			for (k = 0; k < largo2 && s2[k] != '\0'; k++)
-			{
-				concatenados[i] = s2[k];
-				i++;
-			}
-			return (concatenados);
-		}
-	
-	return (NULL);
+	for (k = 0; k < largo2 && s2[k] != '\0'; k++)
+	{
+		concatenados[i] = s2[k];
+		i++;
+	}
+	return (concatenados);
 }
