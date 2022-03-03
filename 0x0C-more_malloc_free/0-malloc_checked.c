22b4ddac7a49ce73b6e2ac385fb2024e429bfe79
Alvaro <ajsabini@gmail.com>
2022-03-03 07:23:43 -0800

    aaa

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * char *malloc_checked - imprimir un string seguido de una linea
+ * @b: el string
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{	
+	void *ret;
+
+	ret = malloc(b);
+	if (ret)
+		return (ret);
+	exit(98);
+	
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * char *string_nconcat - concatenar strings
+ * @s1: el string 1
+ * @s2: el string 2
+ * @n: cant
+ * Return: void
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concatenados;
+	unsigned int largo1 = 0, largo2 = 0, j = 0, i = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	
+	largo1 = strlen(s1);
+	largo2 = strlen(s2);
+
+	if (n >= largo2)
+		largo2 = n;
+	concatenados = malloc(largo1 + largo2 + 1);
+
+	if (concatenados != NULL)
+	{
+		for (i = 0; s1[i];i++)
+			concatenados[i] = s1[i];
+		for (j = 0; j <= n; j++)
+		{
+			concatenados[i] = s2[j];
+			i++;
+		}
+	}	
+	return (concatenados);
+
+}
