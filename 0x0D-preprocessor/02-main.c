2fc28f5e2fd40b8bb0ed639920f2d1d489f73472
Alvaro <ajsabini@gmail.com>
2022-03-08 05:46:14 -0800

    ej 0-4

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * imprimeNombreArchivo - check the code
+ * Return: Always 0.
+ */
+void imprimeNombreArchivo()
+{
+	printf(" %s\n",__FILE__);
+}
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	imprimeNombreArchivo();
+	return (0);
+}
