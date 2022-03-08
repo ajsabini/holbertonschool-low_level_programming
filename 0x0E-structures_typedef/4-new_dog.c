5c09a9583860e36109fb032999e75eabaeaa3b26
Alvaro <ajsabini@gmail.com>
2022-03-08 12:44:45 -0800

    aaa

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,6 +19,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (dogi)
 	{
+		
 		dogi->name = strdup(name);
 		if (!dogi->name)
 		{
@@ -29,7 +30,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		dogi->age = age;
 		dogi->owner = strdup(owner);
 		if (!dogi->owner)
-		{
+		{	
+			free(dogi->name);
 			free(dogi);
 			return (NULL);
 		}
