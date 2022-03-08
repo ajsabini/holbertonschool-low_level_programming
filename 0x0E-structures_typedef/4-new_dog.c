5b433fd9061acc1b74605172ddf81fca39ea145c
Alvaro <ajsabini@gmail.com>
2022-03-08 12:55:29 -0800

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,27 +14,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dogi = NULL;
+	int i;
 
 	dogi = malloc(sizeof(dog_t));
 
 	if (dogi)
 	{
-		
-		dogi->name = strdup(name);
-		if (!dogi->name)
+		dogi->name = malloc(strlen(name) + 1);
+		if(dogi->name)
+		{
+			for(i = 0; name[i]; i++)
+				dogi->name[i] = name[i];
+			dogi->name[i] = '\0';		
+		}
+		else
 		{
 			free(dogi);
 			return (NULL);
 		}
-
+		
 		dogi->age = age;
-		dogi->owner = strdup(owner);
-		if (!dogi->owner)
-		{	
+
+		dogi->owner = malloc(strlen(owner) + 1);
+                if(dogi->owner)
+                {
+                        for(i = 0; owner[i]; i++)
+                                dogi->owner[i] = owner[i];
+                        dogi->owner[i] = '\0';
+                }
+                else
+                {
 			free(dogi->name);
-			free(dogi);
-			return (NULL);
-		}
+                        free(dogi);
+                        return (NULL);
+                }
 	}
 	return (dogi);
 }
