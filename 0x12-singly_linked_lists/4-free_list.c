83ed627234c9627e13fd19c880a5c4bc100e03e0
Alvaro <ajsabini@gmail.com>
2022-03-18 16:56:49 -0700

    ej 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - haer un free de la lista list_t.
+ * @head: nodo head
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *aux = head;
+	list_t *tofree;
+
+	for (; aux;)
+	{
+		tofree = aux;
+		aux = aux->next;
+		free(tofree->str);
+		free(tofree);
+	}
+}
