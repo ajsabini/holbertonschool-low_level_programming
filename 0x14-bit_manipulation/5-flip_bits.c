a28300fb8bc9a9c9d5ac55c1f18f3e4794cdead8
Alvaro <ajsabini@gmail.com>
2022-03-25 18:54:59 -0700

    ej 5 falta terminar

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * flip_bits - asd
+ * @m: decimal
+ * @n: decimal
+ * Return: el total
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i;
+	return (i);
+}
