dc9266df20e1cfad60f1a9c571186b8490672606
Alvaro <ajsabini@gmail.com>
2022-05-11 14:44:44 -0700

    ej 1-2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * add_dnodeint - agregar un nuevo nodeo al principio de la dlinked
+ * @n: el entero a agregar
+ * @head: el head de la lista
+ * Return: NULL si falla, sino la direccion de mem del nuevo nodo
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		dprintf(2, "Error: Can't malloc\n");
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		(*head)->prev = new;
+		new->next = *head;
+		*head = new;
+	}
+	return (new);
+}
