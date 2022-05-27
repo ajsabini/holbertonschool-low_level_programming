81d6d2bb2a719ece6cd465ddbd9682d84d24cc9d
Alvaro <ajsabini@gmail.com>
2022-05-27 06:44:39 -0700

    as

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,14 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_ht;
+	hash_table_t *new_ht = NULL;
+	unsigned long int i = 0;
 
 	new_ht = malloc(sizeof(hash_table_t));
 	if (new_ht == NULL)
 		return (NULL);
-
-	new_ht->array = malloc(size * sizeof(hash_node_t));
+	
+	new_ht->array = malloc(size * sizeof(hash_node_t *));
 	if (new_ht->array == NULL)
 	{
 		free(new_ht);
@@ -22,5 +23,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	new_ht->size = size;
+
+	for (i = 0; i < size; i++)
+		new_ht->array[i] = NULL;
+
 	return (new_ht);
 }
