aebcf03212f9424e05b9c7e9f114704382fa4983
Alvaro <ajsabini@gmail.com>
2022-05-27 06:37:13 -0700

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,8 +14,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_ht == NULL)
 		return (NULL);
 
-	new_ht->array = malloc(size);
-	if (new->ht->array == NULL)
+	new_ht->array = malloc(size * sizeof(hash_node_t));
+	if (new_ht->array == NULL)
 	{
 		free(new_ht);
 		return (NULL);
